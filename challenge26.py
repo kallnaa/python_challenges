02ceee54daf225dcf0e076aaf392bb832a9a8656
Sonia <142484966+kallnaa@users.noreply.github.com>
2025-07-11 06:56:46 -0700

    Fundamentals

---
+++ challenge26.py
@@ -0,0 +1,5 @@
+#Given two integers a and b, which can be positive or negative, find the sum of all the integers between and including them and return it. If the two numbers are equal return a or b.
+#Note: a and b are not ordered!
+
+def get_sum(a,b):
+    return sum(range(min(a, b),max(a, b)+1)) if a !=b else a
